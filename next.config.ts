8efc79031d8c843851e42e3d824ab1bdcf483c0c
Ricardo Martins <desenvolvedor01@israelfrota.com.br>
2026-08-12 17:22:46 -0300

    setup

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/search-company",
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
